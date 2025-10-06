308656ceb8f17d99496db8814eabd9ae5cf595b4
AndreyMih159753 <andreyka_mih@mail.ru>
2025-10-06 19:03:45 +0300

    Добавлен и настроен TailwindCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,29 @@
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
 
   $development: {
-    devtools: { enabled: true }
+    devtools: { enabled: false }
+  },
+
+  $production: {
+    devtools: { enabled: false }
   },
 
   target: 'static',
 
   generate: {
     fallback: true
+  },
+
+  css: [
+    '~/assets/css/main.css'
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss()
+    ]
   }
 })
