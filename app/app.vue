8cfeb4afa8094c7430ef6e5742acabc2eae22d8c
AndreyMih159753 <andreyka_mih@mail.ru>
2025-10-13 20:55:11 +0300

    Сделан header (без адаптации)

--- app/app.vue
+++ app/app.vue
@@ -3,3 +3,23 @@
         <NuxtPage />
     </NuxtLayout>
 </template>
+
+<style>
+@font-face {
+    font-family: 'FrankfurterM';
+    src: url("../app/assets/fonts/FrankfurterM.WOFF");
+}
+
+@font-face {
+    font-family: 'FrankfurterB';
+    src: url("../app/assets/fonts/FrankfurterB.WOFF");
+}
+
+body {
+    background-image: url("../app/assets/images/Background.webp");
+    background-repeat: no-repeat;
+    background-attachment: fixed;
+    background-position: 50%;
+    background-size: cover;
+}
+</style>
--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,3 +1,157 @@
 <template>
-    
+    <div class="max-w-[1140px] mx-auto relative">
+        <div class="flex">
+            <div class="mt-[45px] relative z-100">
+                <NuxtLink to="/">
+                    <img src="../assets/images/Logo.webp" alt="Logo">
+                </NuxtLink>
+            </div>
+        </div>
+        <div>
+            <div class="flex absolute gap-15 items-center top-[25px] right-4.5 z-90">
+                <div>
+                    <ul class="flex gap-5">
+                        <li>
+                            <NuxtLink>
+                                <div class="w-40 h-12.5 bg-[#7F53C3] rounded-[100px]">
+                                    <span class="flex justify-center uppercase text-white text-[26px] leading-[55px]"
+                                        style="font-family: 'FrankfurterM';">
+                                        Parents
+                                    </span>
+                                </div>
+                            </NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink>
+                                <div class="w-40 h-12.5 bg-[#7F53C3] rounded-[100px]">
+                                    <span class="flex justify-center uppercase text-white text-[26px] leading-[55px]"
+                                        style="font-family: 'FrankfurterM';">
+                                        Shop
+                                    </span>
+                                </div>
+                            </NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+                <div>
+                    <ul class="flex gap-5">
+                        <li>
+                            <NuxtLink>
+                                <div class="bg-[#7F53C3] rounded-[100px] size-11 text-center">
+                                    <span class="text-[25px] leading-[41px] text-white">
+                                        <i class="fa fa-facebook"></i>
+                                    </span>
+                                </div>
+                            </NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink>
+                                <div class="bg-[#7F53C3] rounded-[100px] size-11 text-center">
+                                    <span class="text-[25px] leading-[41px] text-white">
+                                        <i class="fa fa-instagram"></i>
+                                    </span>
+                                </div>
+                            </NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="https://www.youtube.com/channel/UCKifwRFflNhWbl1821nm_sg">
+                                <div class="bg-[#7F53C3] rounded-[100px] size-11 text-center">
+                                    <span class="text-[25px] leading-[41px] text-white">
+                                        <i class="fa fa-youtube-play"></i>
+                                    </span>
+                                </div>
+                            </NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink>
+                                <div class="bg-[#7F53C3] rounded-[100px] size-11 text-center">
+                                    <span class="text-[25px] leading-[41px] text-white">
+                                        <i class="fa fa-twitter"></i>
+                                    </span>
+                                </div>
+                            </NuxtLink>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="absolute top-[95px] left-50 p-0">
+                <nav>
+                    <ul
+                        class="flex border-12 border-solid border-white rounded-[90px] ml-[-73px] mr-[18px] pl-15 pb-0.5">
+                        <li
+                            class="w-[332px] h-25 bg-[#FCAF1A] bg-linear-90 from-[#FCAF1A] from-25% to-[#F6E251] [box-shadow:2px_2px_1px_0_rgba(0,0,0,0.50)] ml-[-100px] rounded-[50px] relative z-10">
+                            <NuxtLink>
+                                <div class="pl-[65px]">
+                                    <div class="flex justify-center mt-2.5 mb-[5px]">
+                                        <img src="../assets/images/Friends_(Icon).webp" alt="Friends (Icon)"
+                                            class="h-[46px]">
+                                    </div>
+                                    <div>
+                                        <span
+                                            class="flex justify-center text-[28px] text-white [text-shadow:2px_2px_1px_rgba(0,0,0,0.3)] leading-6.5"
+                                            style="font-family: 'FrankfurterB';">
+                                            Friends
+                                        </span>
+                                    </div>
+                                </div>
+                            </NuxtLink>
+                        </li>
+                        <li
+                            class="w-[332px] h-25 bg-[#00A4DE] bg-linear-90 from-[#00A4DE] from-26% to-[#89C4E9] [box-shadow:2px_2px_1px_0_rgba(0,0,0,0.50)] ml-[-100px]  rounded-[50px] relative z-9">
+                            <NuxtLink>
+                                <div class="pl-[65px]">
+                                    <div class="flex justify-center mt-2.5 mb-[5px]">
+                                        <img src="../assets/images/Activities_(Icon).webp" alt="Activities (Icon)"
+                                            class="h-[46px]">
+                                    </div>
+                                    <div>
+                                        <span
+                                            class="flex justify-center text-[28px] text-white [text-shadow:2px_2px_1px_rgba(0,0,0,0.3)] leading-6.5"
+                                            style="font-family: 'FrankfurterB';">
+                                            Activities
+                                        </span>
+                                    </div>
+                                </div>
+                            </NuxtLink>
+                        </li>
+                        <li
+                            class="w-[332px] h-25 bg-[#EE5799] bg-linear-90 from-[#EE5799] from-29% to-[#F496BE] [box-shadow:2px_2px_1px_0_rgba(0,0,0,0.50)] ml-[-100px]  rounded-[50px] relative z-8">
+                            <NuxtLink>
+                                <div class="pl-[65px]">
+                                    <div class="flex justify-center mt-2.5 mb-[5px]">
+                                        <img src="../assets/images/Watch_(Icon).webp" alt="Watch (Icon)"
+                                            class="h-[46px]">
+                                    </div>
+                                    <div>
+                                        <span
+                                            class="flex justify-center text-[28px] text-white [text-shadow:2px_2px_1px_rgba(0,0,0,0.3)] leading-6.5"
+                                            style="font-family: 'FrankfurterB';">
+                                            Watch
+                                        </span>
+                                    </div>
+                                </div>
+                            </NuxtLink>
+                        </li>
+                        <li
+                            class="w-[332px] h-25 bg-[#421A67] bg-linear-90 from-[#6F6BA0] from-30% to-[#7F4FC6] [box-shadow:2px_2px_1px_0_rgba(0,0,0,0.50)] ml-[-100px]  rounded-[50px] relative z-7">
+                            <NuxtLink>
+                                <div class="pl-[65px]">
+                                    <div class="flex justify-center mt-2.5 mb-[5px]">
+                                        <img src="../assets/images/Play_(Icon).webp" alt="Play (Icon)" class="h-[46px]">
+                                    </div>
+                                    <div>
+                                        <span
+                                            class="flex justify-center text-[28px] text-white [text-shadow:2px_2px_1px_rgba(0,0,0,0.3)] leading-6.5"
+                                            style="font-family: 'FrankfurterB';">
+                                            Play
+                                        </span>
+                                    </div>
+                                </div>
+                            </NuxtLink>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
 </template>
